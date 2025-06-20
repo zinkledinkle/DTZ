7875ff0d8c538c083e17ce083780ae7b3958afe9
zinkledinkle <zgieseler@gmail.com>
2025-06-20 19:47:44 -0400

    shroom generation adjustments

--- Systems/ShroomGen.cs
+++ Systems/ShroomGen.cs
@@ -21,26 +21,31 @@ namespace DTZ.Systems
         private static int[] shroomTileList = shroomTileDict.Keys.ToArray();
         public override void PostWorldGen()
         {
-            PlaceShrooms(200, 200, 150);
+            PlaceShrooms(30, 75, 50); //feel free to adjust these rates later btw
         }
         public override void PreUpdateWorld()
         {
+            if ((int)Main.time % 3600 == 0)
+            {
+                PlaceShrooms(90, 200, 150);
+            }
         }
         public static void PlaceShrooms(int hellcapChance, int toadstoolChance, int iceliumChance)
         {
             int[] chances = { hellcapChance, toadstoolChance, iceliumChance };
-            int[] amount = { 0, 0, 0 };
+            //int[] amount = { 0, 0, 0 };
             for (int x = 0; x < Main.maxTilesX; x++)
             {
                 for (int y = 0; y < Main.maxTilesY; y++)
                 {
-                    if (Framing.GetTileSafely(x, y - 1).HasTile) continue; //obviously don't spawn if theres already a block occupying it
                     Tile tile = Framing.GetTileSafely(x, y);
                     if (!tile.HasTile || tile.LiquidAmount > 0) continue;
+                    if (Framing.GetTileSafely(x, y - 1).HasTile) continue; //obviously don't spawn if theres already a block occupying it
 
-                    int index = 0;
+                    int index = -1;
                     foreach (var shroom in shroomTileDict)
                     {
+                        index++;
                         if (!Main.rand.NextBool(chances[index])) continue;
 
                         int shroomTile = shroom.Key;
@@ -52,15 +57,15 @@ namespace DTZ.Systems
                         {
                             WorldGen.PlaceTile(x, y-1, shroomTile, true, true);
                             NetMessage.SendTileSquare(-1, x, y);
-                            amount[index]++;
+                            //amount[index]++;
                         }
-                        index++;
+
                     }
                 }
             }
-            Main.NewText("Placed " + amount[0] + " hellcaps!");
+            /*Main.NewText("Placed " + amount[0] + " hellcaps!");
             Main.NewText("Placed " + amount[1] + " toadstools!");
-            Main.NewText("Placed " + amount[2] + " iceliums!");
+            Main.NewText("Placed " + amount[2] + " iceliums!");*/ //was using for debugging earlier, uncomment if you wanna test spawns
         }
     }
 }
