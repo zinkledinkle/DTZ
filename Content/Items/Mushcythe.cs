084c98ace2ab5ea39eb9e68cf180021dc197234f
zinkledinkle <zgieseler@gmail.com>
2025-06-20 17:26:42 -0400

    stuff

---
+++ Content/Items/Mushcythe.cs
@@ -0,0 +1,41 @@
+﻿using DTZ.Content.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Items
+{
+    public class Mushcythe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 38;
+            Item.damage = 8;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.value = Item.buyPrice(silver: 10);
+        }
+        public override bool? UseItem(Player player)
+        {
+            Point16 tileCoords = Main.MouseWorld.ToTileCoordinates16();
+            Tile tile = Framing.GetTileSafely(tileCoords);
+            if (tile.TileType == TileID.Mud || tile.TileType == TileID.MushroomGrass)
+            {
+                tile.TileType = (ushort)ModContent.TileType<TilledMud>();
+            }
+            return true;
+        }
+    }
+}
---
+++ Content/Tiles/TilledMud.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Tiles
+{
+    public class TilledMud : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMerge[Type][TileID.Mud] = true;
+            Main.tileMerge[Type][TileID.MushroomGrass] = true;
+            Main.tileMerge[TileID.Mud][Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            VanillaFallbackOnModDeletion = TileID.Mud;
+            MineResist = 0.6f;
+            HitSound = SoundID.Dig;
+
+            DustType = DustID.Mud;
+
+            AddMapEntry(new Color(314, 247, 271), Language.GetText("TilledMud"));
+        }
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 2 : 5;
+        }
+    }
+}
