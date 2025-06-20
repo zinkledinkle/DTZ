9c1d71cdd9588be54cc1c4fec5012674f3f59c7a
zinkledinkle <zgieseler@gmail.com>
2025-06-20 11:02:45 -0400

    guh!

--- DTZ.cs
+++ DTZ.cs
@@ -7,7 +7,6 @@ using Terraria.ModLoader;
 
 namespace DTZ
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class DTZ : Mod
 	{
 
