869d68f57e29d7d07acc15303877dac21dca208f
zinkledinkle <zgieseler@gmail.com>
2025-06-20 20:11:42 -0400

    name

---
+++ Content/Items/MushionSeeds.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Items
+{
+    public class MushionSeeds : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 24;
+            Item.rare = ItemRarityID.Blue;
+            Item.maxStack = 9999;
+            Item.value = Item.buyPrice(silver: 50);
+            Item.DefaultToPlaceableTile<>();
+        }
+    }
+}
