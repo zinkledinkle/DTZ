5aac8c15e8846f4e1bbe1aef5888955a077fb6d2
Exslueterra <140874099+Exslueterra@users.noreply.github.com>
2025-06-22 18:53:55 -0500

    Create Devnote.cs
    cs file for the devnote

---
+++ Content/Items/Devnote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Items
+{
+    public class Devnote : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 20;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(silver: 5);
+            Item.maxStack = 9999;
+        }
+    }
+}
