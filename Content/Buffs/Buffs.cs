39dcfcdd42a5f254680b063bbd0267c424024c19
Lucid Lizard <danielrpankratz@gmail.com>
2025-06-23 16:07:23 -0500

    mushion buff fix
    Mushion buffs now scale with owned mushion counts

--- Content/Buffs/Buffs.cs
+++ Content/Buffs/Buffs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DTZ.Content.Projectiles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace DTZ.Content.Buffs
     {
         public override void Update(Player player, ref int buffIndex)
         {
-            player.lifeRegen += 5;
+            player.lifeRegen += 5 * player.ownedProjectileCounts[ModContent.ProjectileType<GlowingMushion>()];
         }
     }
 
@@ -22,22 +23,22 @@ namespace DTZ.Content.Buffs
     {
         public override void Update(Player player, ref int buffIndex)
         {
-            player.GetCritChance(DamageClass.Generic) += 5;
+            player.GetCritChance(DamageClass.Generic) += 2 * player.ownedProjectileCounts[ModContent.ProjectileType<HellcapMushion>()];
         }
     }
     public class IceliumBuff : ModBuff
     {
         public override void Update(Player player, ref int buffIndex)
         {
-            player.DefenseEffectiveness *= .90f; 
+            player.DefenseEffectiveness *= 1f - (.075f *player.ownedProjectileCounts[ModContent.ProjectileType<IceliumMushion>()]); 
         }
     }
     public class ToadBuff : ModBuff
     {
         public override void Update(Player player, ref int buffIndex)
         {
-            player.moveSpeed += .3f;
-            player.jumpSpeedBoost += 3;
+            player.moveSpeed += .3f * player.ownedProjectileCounts[ModContent.ProjectileType<ToadMushion>()];
+            player.jumpSpeedBoost += 1 * player.ownedProjectileCounts[ModContent.ProjectileType<ToadMushion>()];
         }
     }
 }
