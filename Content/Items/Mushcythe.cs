c833d5669cfef553dc8ff23684cc0d4c68678e22
Lucid Lizard <danielrpankratz@gmail.com>
2025-06-23 00:53:39 -0500

    Glowing Mushion
    adds the glowing mushion minion.
    fixes a bug with mushcythe where you were locked in a direction while using

--- Content/Items/Mushcythe.cs
+++ Content/Items/Mushcythe.cs
@@ -130,6 +130,7 @@ namespace DTZ.Content.Items
             Item.rare = ItemRarityID.Blue;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.value = Item.buyPrice(silver: 10);
+            Item.useTurn = true;
         }
         public override bool? UseItem(Player player)
         {
