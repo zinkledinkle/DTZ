8c90751bf6e75b96c9e815bebdfd765872c15c9d
Exslueterra <140874099+Exslueterra@users.noreply.github.com>
2025-07-08 05:27:43 -0500

    camel case (fixed an error on the normie mushion's buffs)

--- Content/Buffs/Buffs.cs
+++ Content/Buffs/Buffs.cs
@@ -59,7 +59,7 @@ namespace DTZ.Content.Buffs
         public override void Update(Player player, ref int buffIndex)
         {
             base.Update(player, ref buffIndex);
-            player.lifeStatMax2 += 30 * player.ownedProjectileCounts[ProjectileType];
+            player.StatlifeMax2 += 30 * player.ownedProjectileCounts[ProjectileType];
         }
         protected override int ProjectileType => ModContent.ProjectileType<NormieMushion>();
     }
