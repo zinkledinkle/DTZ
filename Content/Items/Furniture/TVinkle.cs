1fd7cf7d1016d6675472b9a28ed45aca2cd7aeba
Exslueterra <140874099+Exslueterra@users.noreply.github.com>
2025-07-24 09:29:13 -0500

    (probably has issues) added tvinkle, zinklite bar, localization changes

---
+++ Content/Items/Furniture/TVinkle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Items.Furniture
+{
+    public class TVinkle : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ExampleTable>());
+            Item.value = 150;
+            Item.maxStack = 99;
+            Item.width = 68;
+            Item.height = 56;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(1)
+                .AddIngredient<ZinkliteBar>(5)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
---
+++ Content/Items/ZinkliteBar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace DTZ.Content.Items
+{
+    public class ZinkliteBar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
+            ItemID.Sets.SortingPriorityMaterials[Item.type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.value = 2000;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+            // Item.createTile = ModContent.TileType<Tiles.ExampleBar>();
+            // Item.placeStyle = 0;
+        }
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<ExampleOre>(4)
+                .AddTile(TileID.Furnaces)
+                .Register();
+        }
+    }
+}
