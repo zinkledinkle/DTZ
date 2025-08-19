adc80c9f19e6a2b008cad707ad970383cb75d55c
Exslueterra <140874099+Exslueterra@users.noreply.github.com>
2025-08-19 00:25:38 -0500

    added all three music boxes (tiles, items, songs)

---
+++ Content/Items/Furniture/PerilBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Mycology.Content.Tiles;
+
+namespace Mycology.Content.Items.Furniture
+{
+    public class PerilBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Music/Peril"), ModContent.ItemType<PerilBox>(), ModContent.TileType<PerilBoxTile>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToMusicBox(ModContent.TileType<PerilBoxTile>(), 0);
+        }
+    }
+}
