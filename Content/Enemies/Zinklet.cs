8c9be060c84973332f51fa5e29948aee9e7b83e0
Exslueterra <140874099+Exslueterra@users.noreply.github.com>
2025-09-18 01:52:08 -0500

    i just.. i just added green grapes.. nothing special.!

---
+++ Content/Enemies/Zinklet.cs
@@ -0,0 +1,77 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using System;
+using Terraria.GameContent.ItemDropRules;
+
+
+namespace Mycology.Content.Enemies
+{
+    public class Zinklet : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = Main.npcFrameCount[2];
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 26;
+            NPC.height = 28;
+            NPC.damage = 15;
+            NPC.defense = 10;
+            NPC.lifeMax = 100;
+            NPC.value = 50f;
+            NPC.aiStyle = 1;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            AIType = NPCID.GreenSlime;
+            AnimationType = NPCID.GreenSlime;
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            if (Math.Abs(NPC.velocity.X) > 0)
+            {
+                NPC.spriteDirection = -Math.Sign(NPC.velocity.X);
+            }
+
+            if (NPC.aiStyle == 1 && NPC.velocity.Y == 0)
+            {
+                NPC.ai[0]--;
+                if (NPC.ai[0] <= 0)
+                {
+                    NPC.velocity.Y = -4;
+                    NPC.ai[0] = 40;
+
+                    SoundEngine.PlaySound(new SoundStyle("Mycology/Assets/Sounds/ZinkletJump"), NPC.position);
+                }
+            }
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter++;
+            if (NPC.frameCounter >= 12)
+            {
+                NPC.frameCounter = 0;
+            }
+            NPC.frame.Y = (int)NPC.frameCounter / 4 * frameHeight;
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.ZinkliteOre>(), 1, 1, 2));
+        }
+
+        public override void OnKill()
+        {
+            SoundStyle myDeathSound = new SoundStyle("Mycology/Assets/Sounds/Oppsgotherscream");
+            SoundEngine.PlaySound(myDeathSound, NPC.position);
+        }
+    }
+}
---
+++ Content/Items/GreenGrapes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Mycology.Content.Buffs;
+using Mycology.Content.Enemies;
+
+namespace Mycology.Content.Items
+{
+    public class GreenGrapes : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 26;
+            Item.useStyle = ItemUseStyleID.EatFood;
+            Item.useAnimation = 17;
+            Item.useTime = 17;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item2;
+            Item.consumable = true;
+            Item.maxStack = 30;
+            Item.value = Item.buyPrice(0, 0, 50, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.buffType = ModContent.BuffType<Buffs.GREEN>();
+            Item.buffTime = 60 * 10;
+        }
+        public override bool? UseItem(Player player)
+        {
+            if (Main.myPlayer == player.whoAmI)
+            {
+                int npcType = ModContent.NPCType<Zinklet>();
+                int npcIndex = NPC.NewNPC(null, (int)player.position.X, (int)player.position.Y, npcType);
+            }
+
+            return true;
+        }
+    }
+}
